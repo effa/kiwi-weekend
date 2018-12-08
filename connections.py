570b315a1e63199e2d8fdf1fceb57ecb14a240ca
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-12-08 15:38:05 +0100

    Cache info in redis

--- connections.py
+++ connections.py
@@ -5,6 +5,7 @@ $ pipenv run python connections.py --source Brno --destination Praha --departure
 import argparse
 import json
 import re
+from redis import StrictRedis
 from requests_html import HTMLSession
 from unidecode import unidecode
 
@@ -21,18 +22,29 @@ def main():
 
 def get_connections(source, destination, departure_date, session=None):
     session = session or HTMLSession()
-    codes_map = get_city_codes(session)
+    redis = StrictRedis(socket_connect_timeout=3, **get_redis_config())
     rates = get_rates(session)
-    source = get_city_code(source, codes_map)
-    destination = get_city_code(destination, codes_map)
+    source = normalize(source)
+    destination = normalize(destination)
+    source_code = get_city_code(source, redis, session)
+    destination_code = get_city_code(destination, redis, session)
+    journey_key = get_redis_journey_key(source, destination, departure_date)
+    journeys = redis.get(journey_key)
+    if journeys:
+        print('Using cached journeys...')
+        return json.loads(journeys.decode('utf-8'))
+    print('Scraping journeys...')
     # The URL is e.g. 'https://www.elines.cz/jizdenky?from=CZE%7EBrno&to=CZE%7EPrague&forth=2018-12-09&back='
-    url = f'https://www.elines.cz/jizdenky?from={source}&to={destination}&forth={departure_date}&back='
+    url = f'https://www.elines.cz/jizdenky?from={source_code}&to={destination_code}&forth={departure_date}&back='
     response = session.get(url)
-    connections_list = response.html.find(f'.day-1[data-date="{departure_date}"]')
-    return [parse_connection_element(el, rates) for el in connections_list]
+    connection_elements = response.html.find(f'.day-1[data-date="{departure_date}"]')
+    connections = [parse_connection_element(el, rates) for el in connection_elements]
+    redis.setex(journey_key, 60*60, json.dumps(connections))
+    return connections
 
 
-def get_city_codes(session):
+def get_and_cache_city_codes(session, redis):
+    print('Scraping city codes...')
     response = session.get('https://elines.cz/cz/')
     cities_elements = response.html.find('#cities option')
     codes_map = {}
@@ -42,6 +54,9 @@ def get_city_codes(session):
         if '~' in code:
             for name in names.split('('):
                 codes_map[normalize(name)] = code
+    for city_name, code in codes_map.items():
+        city_key = get_redis_location_key(city_name)
+        redis.setex(city_key, 60*60, code)
     return codes_map
 
 
@@ -52,14 +67,26 @@ def normalize(name):
     return name
 
 
-def get_city_code(city_name, codes_map):
+def get_city_code(city_name, redis, session):
+    city_name = normalize(city_name)
+    city_code = redis.get(get_redis_location_key(city_name))
+    if city_code:
+        return city_code.decode('utf-8')
+    codes_map = get_and_cache_city_codes(session, redis)
     return codes_map[normalize(city_name)]
 
+
+def get_redis_location_key(city_name):
+    return f'location:{city_name}_eurolines'
+
+
+def get_redis_journey_key(source, destination, departure_date):
+    return f'journey:{source}_{destination}_{departure_date}_eurolines'
 
 def parse_connection_element(connection_element, rates):
     departure = connection_element.find('section.departure > span')[0].text
     arrival = connection_element.find('section.arrival > span')[0].text
-    price_czk = float(connection_element.find('section.price .basic .amount')[0].text)
+    price_czk = min(float(el.text) for el in connection_element.find('section.price .amount'))
     price = czk_to_eur(price_czk, rates)
     return {'departure': departure, 'arrival': arrival, 'price': price}
 
@@ -71,6 +98,16 @@ def get_rates(session):
 
 def czk_to_eur(czk, rates):
     return rates['CZK'] * czk
+
+
+def get_redis_config():
+    return {
+        'host': '142.93.160.67',
+        'password': 'akd89DSk23Kldl0ram29',
+        'port': 6379,
+    }
+
+
 
 
 if __name__ == "__main__":
--- test_connections.py
+++ test_connections.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock
 from requests_html import HTMLSession
+import requests
 import connections
 
 
@@ -9,7 +10,14 @@ def test_normalize():
 
 #def test_get_connections():
 #    session = HTMLSession()
-#    # TODO: return current content for the 3 urls
-#    session.get = MagicMock(side_effect=)
+#    def fake_session_get(url):
+#        response = requests.Response()
+#        if url == 'https://api.skypicker.com/rates':
+#            response.json = MagicMock(return_value={"CZK":0.0386738})
+#        # TODO: return current content for the 3 urls
+#        elif url == 'https://elines.cz/cz/':
+#            response._content = 'todo'.encode('utf-8')
+#        return response
+#    session.get = MagicMock(side_effect=fake_session_get)
 #    results = connections.get_connections('Brno', 'Praha', '2018-12-09', session=session)
 #    assert results == [{"departure": "13:45", "arrival": "16:15", "price": 8.314867}]
