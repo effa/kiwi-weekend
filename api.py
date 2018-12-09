af4031f2a00cf90e5f1b111c2e18b2c707e48784
Tomáš Effenberger <Tomas.Effenberger@gmail.com>
2018-12-09 10:45:46 +0100

    Refactoring

--- api.py
+++ api.py
@@ -6,24 +6,29 @@ app = Flask(__name__)
 
 @app.route('/api/search', methods=['GET'])
 def search():
-    # TODO: Extend the API to allow from date_from, date_to (first fix it).
-    source = request.args.get('source')
-    destination = request.args.get('destination')
-    date = request.args.get('date')
-    if not source or not destination or not date:
-        # TODO: Error response
-        return 'Missing param, expects source, destination, and date.'
-    results = connections.get_connections(source, destination, date)
-    return jsonify(results)
+    try:
+        search_args = get_search_request_args()
+    except ValueError as exc:
+        return str(exc)  # TODO: Error response
+    journeys = connections.get_connections_interval(*search_args)
+    return jsonify(journeys)
 
 
 @app.route('/search', methods=['GET'])
 def search_view():
+    try:
+        search_args = get_search_request_args()
+    except ValueError as exc:
+        return str(exc)  # TODO: Error response
+    journeys = connections.get_connections_interval(*search_args)
+    return render_template('results.html', journeys=journeys)
+
+
+def get_search_request_args():
     source = request.args.get('source')
     destination = request.args.get('destination')
-    date = request.args.get('date')
-    if not source or not destination or not date:
-        # TODO: Error response
-        return 'Missing param, expects source, destination, and date.'
-    journeys = connections.get_connections(source, destination, date)
-    return render_template('results.html', journeys=journeys)
+    date_from = request.args.get('date_from')
+    date_to = request.args.get('date_to')
+    if not source or not destination or not date_from or not date_to:
+        raise ValueError('Missing param. Expects source, destination, date_from, date_to.')
+    return source, destination, date_from, date_to
